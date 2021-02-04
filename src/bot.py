fd5c18efafe28e2b49bd96e29d7953eba5f00492
Ian Effendi <effendiian@gmail.com>
2021-02-04 00:38:29 -0500

    Add bot basics.

--- src/bot.py
+++ src/bot.py
@@ -7,20 +7,29 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client - discord.Client()
+client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event
 async def on_message(message):
-    if message.author == client.user
+    if message.author == client.user:
         return
+    if message.content.startswith('$'):
+        await handle_command(message)
 
+
+async def handle_command(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    if message.content.startswith('$goodbye'):
+        await message.channel.send('No...')
 
+# Run the client.
 client.run(TOKEN)
 
 
